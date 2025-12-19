e8b288f80be58d0f4b25dec17732a3f0f8db2cdd
KaramAlBatta <Karam.Al-Batta@Student.HTW-Berlin.de>
2025-12-19 11:27:56 +0100

    M4 Milestone 4 neu

---
+++ src/main/java/To_Do_Liste/webtech/WebConfig.java
@@ -0,0 +1,27 @@
+package To_Do_Liste.webtech;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "https://to-do-app-1-l2tv.onrender.com", // ← dein FRONTEND!
+                                "http://localhost:5173"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
